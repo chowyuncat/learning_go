6b42d5dbfe1ec6c252bd308b80e4fab8756cd3c1
Cat Zimmermann <chowyuncat@chowyuncat.com>
2022-04-25 16:52:12 -0700

    Add two naive implementations of a prime number generator.
    One is a channel generator and the other is a closure-only generator.

---
+++ learn_generator/generateprimes.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+)
+
+func MakeGeneratorChannel() chan int {
+	primes := make(chan int)
+
+	go func() {
+
+		primes <- 1
+		primes <- 2
+
+		for candidate := 3; true; candidate++ {
+
+			i := 2
+			for ; i < candidate; i++ {
+				if candidate%i == 0 {
+					break
+				}
+			}
+
+			if i == candidate {
+				primes <- candidate
+			}
+
+		}
+	}()
+
+	return primes
+}
+
+func MakeGeneratorTraditional() func() int {
+
+	candidate := 0
+
+	getprime := func() int {
+
+		for {
+
+			candidate++
+
+			if candidate < 3 {
+				return candidate
+			}
+
+			i := 2
+			for ; i < candidate; i++ {
+
+				if candidate%i == 0 {
+					break
+				}
+			}
+
+			if i == candidate {
+				return candidate
+			}
+		}
+	}
+
+	return getprime
+}
+
+func main() {
+	N, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var sum int
+
+	// runtime.GOMAXPROCS(1)
+	fmt.Printf("Limited to %d cpus\n", runtime.GOMAXPROCS(0))
+
+	loops := 0
+
+	if false {
+		primes := MakeGeneratorChannel()
+
+		i := 0
+		for prime := range primes {
+			loops++
+			sum += prime
+			i++
+			if i >= N {
+				break
+			}
+		}
+	}
+
+	if true {
+		getprime := MakeGeneratorTraditional()
+		for i := 0; i < N; i++ {
+			loops++
+			prime := getprime()
+			sum += prime
+		}
+	}
+
+	fmt.Printf("sum=%d, loops=%d\n", sum, loops)
+}
